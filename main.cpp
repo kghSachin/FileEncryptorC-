6b510e32075b1c391dea13e1836e9da08c26357e
sachin <118202383+kghSachin@users.noreply.github.com>
2024-07-06 23:10:26 +0545

    filesystem added

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <filesystem>
+#include "./src/app/processes/processManagement.hpp"
+#include "./src/app/processes/Task.hpp"
+
+// namespace fs = std::filesystem;
+
+int main()
+{
+    std::cout << "Hello World\n";
+    return 0;
+}
--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <sstream>
 #include <iostream>
-#include "IO.hpp"
+#include "../fileHandling/IO.hpp"
 
 enum class Action
 {
